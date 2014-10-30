72f6d46c193513d338bab40858a256fb9b528523
Nilno <ajread@fullsail.edu>
2014-10-29 18:25:19 -0600

    Prompted user for name
    Prompted user for name

--- Setup GitHub/js/script.js
+++ Setup GitHub/js/script.js
@@ -3,4 +3,11 @@ Amaren Read
 Test JS file
 10/29/2014
  */
+
 //alert ("Testing 1, 2, 3 ")
+
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name");
+console.log(userInput);
