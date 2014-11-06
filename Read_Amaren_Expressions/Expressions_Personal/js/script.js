5d1e6d3622763347fbe52383ff11be12851b5c5f
Nilno <ajread@fullsail.edu>
2014-11-06 01:58:49 -0700

    Asked user their name and saved money
    Asked the user for their name and how much money they have saved up for
    new hardware for personal expression.

--- Read_Amaren_Expressions/Expressions_Personal/js/script.js
+++ Read_Amaren_Expressions/Expressions_Personal/js/script.js
@@ -6,4 +6,24 @@
  */
 //alert("Testing 1,2,3");
 
+//Create a js file that will tell how much money is needed to buy a new mouse
+
+//Ask the user their name
+
+var name = prompt("Type in your name please:");
+
+//Hi to the user
+
+alert("Hi "+name+"! Let's see how much money you have toward new hardware")
+
+//Ask how much they have saved
+
+var moneySaved = prompt("How much money do you have saved toward new hardware?");
+
+//Print out response in console
+
+console.log(moneySaved);
+
+
+
 
