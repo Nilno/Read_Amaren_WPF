6ede0bfda2548637f9667f98d5ac91746febb80f
Nilno <ajread@fullsail.edu>
2014-11-05 19:42:39 -0700

    completed slice of pie I & II
    wrote expressions to figure out how many slices of pizza the party
    goers get individually, added ; on var lines on console.log lines and
    see how many slices Sparky gets.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -5,7 +5,7 @@
  11-4-14
  */
 
-//Sparky's age
+//Sparky's age in human years
 
 var age = 1;
 
@@ -15,8 +15,70 @@ var age = 7;
 
 //Sparky is 1 years old in human years and 7 years old in dog years
 
-age = 1 * 7;
+var age = 1 * 7;
 
 
-console.log(age);
+//console.log(age);
 
+
+//Slice of pie I
+
+//How many party goers
+
+var partyGoers = 10;
+
+//How many pizzas ordered
+
+var pizzaOrder = 6;
+
+//How many slices each pizza has
+
+var slicePizza = 8;
+
+//How many slices per pizza ordered
+
+slices = slicePizza * pizzaOrder;
+
+//console.log(slices)
+
+//How many slices party goers get
+
+var partyGoers = slices/partyGoers
+
+//console.log(partyGoers)
+
+
+//Slice of pie II
+
+//how many party goers
+
+var partyGoers = 10;
+
+//How many pizzas ordered
+
+var pizzaOrder = 6;
+
+//How many slices each pizza has
+
+var slicePizza = 8;
+
+//How many slices per pizza ordered
+
+slices = slicePizza * pizzaOrder;
+
+console.log(slices);
+
+//How many slices party goers get
+
+var partyGoers = slices/partyGoers;
+
+console.log(partyGoers)
+
+//Remainder of left over pizza
+
+var remainder = 0.8
+
+//How much pizza does Sparky get
+var sparky = remainder * partyGoers;
+
+console.log(sparky);
