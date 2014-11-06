35611a4e1bbd028715affdedfa112968906314c8
Nilno <ajread@fullsail.edu>
2014-11-06 02:59:44 -0700

    Completed Personal Expressions
    Alerted and prompted user how much they will need in order to by a new
    mouse with what money they already saved up.

--- Read_Amaren_Expressions/Expressions_Personal/js/script.js
+++ Read_Amaren_Expressions/Expressions_Personal/js/script.js
@@ -14,11 +14,17 @@ var name = prompt("Type in your name please:");
 
 //Hi to the user
 
-alert("Hi "+name+"! Let's see how much money you have toward new hardware")
+alert("Hi "+name+"! Let's see how much money you have toward a new mouse.")
+
+console.log("Hi "+name+"! Let's see how much money you have toward a new mouse.")
+
+//Prompt user to see what type of mouse they would like to get
+
+prompt("What type of mouse would you like to buy?");
 
 //Ask how much they have saved
 
-var moneySaved = prompt("How much money do you have saved toward new hardware?");
+var moneySaved = prompt("How much money do you have saved toward a new mouse?");
 
 //Print out response in console
 
@@ -26,14 +32,22 @@ console.log(moneySaved);
 
 //Take savings and subtract it from mouse price
 
-var mousePrice = 40.00;
-
-//Calculate how much money is needed from savings
-
-var moneyNeeded = mousePrice - moneySaved;
+var mousePrice = 40;
 
 //Alert user of how much is saved
 
 alert(name+" you have " +moneySaved+" right now.");
 
+console.log(name+" you have " +moneySaved+" right now.");
 
+//Calculate how much money is needed toward savings
+
+var moneyNeeded = mousePrice - moneySaved;
+
+console.log(moneyNeeded);
+
+//Alert user how much they will need
+
+alert(name+" you will need " +moneyNeeded+" for the new hardware.");
+
+console.log(name+" you will need " +moneyNeeded+" for the new hardware.");
