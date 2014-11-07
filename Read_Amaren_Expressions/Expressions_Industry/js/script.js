e842deaa28fb6f5f956fce33f806b06cc1bd444d
Nilno <ajread@fullsail.edu>
2014-11-06 21:01:17 -0700

    Finished with assignment
    Finished with assignment

--- Read_Amaren_Expressions/Expressions_Industry/js/script.js
+++ Read_Amaren_Expressions/Expressions_Industry/js/script.js
@@ -21,3 +21,46 @@ alert("Hello "+name+"! Let's see how much soil you will need to plant a tree.")
 
 console.log("Hello "+name+"! Let's see how much soil you will need to plant a tree.")
 
+//Prompt user to see how much soil they have
+
+var haveSoil = prompt("How much soil do u have "+name+" ?");
+
+//Create array to see total amount of soil
+
+var soilBins = [20, 30];
+
+//Calculate wood total amount of soil
+
+var soilTotal = soilBins[0] + soilBins[1];
+
+//Alert user stating how much soil there is.
+
+alert("You have "+soilTotal+ " amounts of soil.");
+
+//Print out total amount of soil from soil pile to console
+
+console.log(soilTotal+ " from soil pile.");
+
+//Alert user to ask if they have the  of soil from the total amount of soil.
+
+alert("You have "+haveSoil+" from "+soilTotal+" amounts from soil pile.");
+
+//Printout of user to ask if they have the  of soil from the total amount of soil.
+
+console.log("You have "+haveSoil+" from "+soilTotal+" amounts soil pile.");
+
+//Calculate total amount of soil subtracted from the amount the user has
+
+var moreSoil = soilTotal - parseInt(haveSoil);
+
+//Print out total amount of soil subtracted from the amount the user has
+
+console.log(moreSoil+ " amounts of soil subtracted from the amount the user has.");
+
+//Alert user how much soil they will need from
+
+alert(name+ " you will need " +moreSoil+" amounts of soil to plant the tree.");
+
+//Print out name and how much soil in order to plant the tree to console
+
+console.log(name+ " you will need " +moreSoil+" amounts of soil to plant the tree.");
