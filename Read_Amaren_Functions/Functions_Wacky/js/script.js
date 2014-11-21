45212f6aa08a9ae34526becca4404c0432ad1077
Nilno <ajread@fullsail.edu>
2014-11-20 20:21:36 -0700

    Editing Functions Wacky
    Editing Functions Wacky

--- Read_Amaren_Functions/Functions_Wacky/js/script.js
+++ Read_Amaren_Functions/Functions_Wacky/js/script.js
@@ -22,4 +22,26 @@ while(name === ""){
 
 //Setup vars for giving the user numbers to flick
 
+//Prompt user asking what number they would like to flick
+
+var firstNum = prompt("Please pick a number between 0 and 20 that you would like to flick first " +name+ " ?");
+
+//Re-prompt user if user input is not a number or user input is blank
+
+while(isNaN(firstNum) || firstNum==="") {
+
+ firstNum = prompt("Please type in a number between 0 and 20 " + name + ".")
+
+}
+
+var highNum = prompt("What is the highest number you would like to flick " +name+ "?");
+
+//Reprompt user if user input is not a number or user input is blank
+
+while(isNaN(highNum) || highNum==="") {
+
+ highNum = prompt(+name+ ", Please type in the highest number you would like to flick.")
+
+}
+
 
