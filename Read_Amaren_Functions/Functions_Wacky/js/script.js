ebeadd696399a61e7e403c675d37e6f87c44baf7
Nilno <ajread@fullsail.edu>
2014-11-20 21:26:28 -0700

    Completed Wacky Function
    Completed Wacky Function

--- Read_Amaren_Functions/Functions_Wacky/js/script.js
+++ Read_Amaren_Functions/Functions_Wacky/js/script.js
@@ -15,7 +15,7 @@ var name = prompt("Will you type in your name please?");
 
 //re-prompt for name of user
 
-while(name === ""){
+while(name=== ""){
 
  name = prompt("Please type in your name.")
 }
@@ -34,14 +34,39 @@ while(isNaN(firstNum) || firstNum==="") {
 
 }
 
-var highNum = prompt("What is the highest number you would like to flick " +name+ "?");
+var highNum = prompt("What is the highest number between 0 and 20 you would like to flick " +name+ "?");
 
 //Reprompt user if user input is not a number or user input is blank
 
 while(isNaN(highNum) || highNum==="") {
 
- highNum = prompt(+name+ ", Please type in the highest number you would like to flick.")
+ highNum = prompt(name+ ", Please type in the highest number between 0 and 10 you would like to flick.")
 
 }
 
+//Call the function
 
+var allNum = flickNum(firstNum, highNum);
+
+//Alert user how much money they have in their pocket
+
+alert(name+ ", you can flick " +allNum+ " these numbers.");
+
+//Function to calculate how much user has in pocket
+
+function flickNum(firstNum, highNum){
+
+//See which numbers user will flick
+
+ var groupNum = Math.round(Math.random(firstNum+highNum));
+
+ //Return var groupNum
+
+ return groupNum;
+}
+
+for(var i=0; i<20; i++){
+
+ console.log(flickNum(firstNum, highNum));
+
+}
