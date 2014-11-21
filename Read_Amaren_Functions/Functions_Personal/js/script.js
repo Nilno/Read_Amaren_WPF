ed8582acada04a1c7e42d5aac87eb13aba8cd01d
Nilno <ajread@fullsail.edu>
2014-11-20 19:31:26 -0700

    Edited Functions Personal
    Edited Functions Personal

--- Read_Amaren_Functions/Functions_Personal/js/script.js
+++ Read_Amaren_Functions/Functions_Personal/js/script.js
@@ -27,3 +27,28 @@ while(name === ""){
 
 alert("We are going to calculate how much money you have in your pocket " +name+ ".");
 
+//Setup vars for calculation
+
+//Prompt user asking how much money they took out of the bank
+
+var withdrawBank = prompt("How much money did u take out of your bank " +name+ " ?");
+
+//Reprompt user if user input is not a number or user input is blank
+
+while(isNaN(withdrawBank) || withdrawBank==="") {
+
+ withdrawBank = prompt("Please type in how much you withdrew from your bank " + name + ".")
+
+}
+
+var paymentOrGave = prompt("Did you pay for anything or gave away any money, if so, how much " +name+ ".");
+
+//Reprompt user if user input is not a number or user input is blank
+
+while(isNaN(paymentOrGave) || paymentOrGave==="") {
+
+ paymentOrGave = prompt("Type in how much you paid or gave away please " + name + ".")
+
+}
+
+
